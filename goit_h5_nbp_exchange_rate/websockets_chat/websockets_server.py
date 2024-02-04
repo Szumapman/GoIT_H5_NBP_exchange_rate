e153b851eec602d7b149b91d7325162503bc2c8e
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-04 22:04:29 +0100

    Added some comments

--- goit_h5_nbp_exchange_rate/websockets_chat/websockets_server.py
+++ goit_h5_nbp_exchange_rate/websockets_chat/websockets_server.py
@@ -16,7 +16,6 @@ from goit_h5_nbp_exchange_rate.utilities.nbp_exchange_rate_tools import (
     pretty_view,
 )
 
-
 # Create a logger with the name "server" and set its level to DEBUG.
 logger = logging.getLogger("server")
 logger.setLevel(logging.DEBUG)
@@ -31,6 +30,9 @@ logger.addHandler(fh)
 
 
 class Server:
+    """
+    Websocket server class from lecture (the only change is in distribute method).
+    """
     clients = set()
 
     async def register(self, ws: WebSocketServerProtocol):
@@ -57,8 +59,10 @@ class Server:
 
     async def distribute(self, ws: WebSocketServerProtocol):
         async for message in ws:
+            # Check if the message is an exchange request.
             if message.split()[0].lower() == "exchange":
                 await exchange_logging(ws.name, message)
+                # Create message with the exchange data.
                 range_of_days, currencies = get_args(message.split()[1:])
                 data_from_nbp = await get_data_from_nbp(range_of_days, currencies)
                 message = data_adapter(data_from_nbp)
@@ -67,6 +71,11 @@ class Server:
 
 
 async def _set_exchange_logging_directory():
+    """
+    Function to set the directory for the exchange logging.
+
+    :return: string with the directory path.
+    """
     app_dir = await aiopath.Path(__file__).parent.absolute()
     logging_directory = app_dir.joinpath("exchange_logging")
     if not await logging_directory.exists():
@@ -75,6 +84,12 @@ async def _set_exchange_logging_directory():
 
 
 async def exchange_logging(user_name: str, command: str):
+    """
+    Function to save in file logging for the exchange command.
+
+    :param user_name: string with user's name who called the command.
+    :param command: full command string.
+    """
     logging_directory = await _set_exchange_logging_directory()
     log_file = logging_directory.joinpath("exchange.log")
     async with aiofile.async_open(log_file, mode="a") as f:
